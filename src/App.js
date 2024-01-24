bb376555c5363ef11088e5ac2d40c55381825398
Alexis Tamayo <lexx426@gmail.com>
2024-01-24 14:23:00 -0600

    Keyboard visuals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
 import './App.css';
+import { boardDefault } from './components/Words'; 
+import { createContext, useState } from 'react';
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault)
   return (
     <div className="App">
-      
+      <nav> <h1> Wordle </h1></nav>
+      <AppContext.Provider value={{board, setBoard}}>
+        <div className='game'>
+          <Board/>
+          <Keyboard/>
+        </div>
+      </AppContext.Provider>
     </div>
   );
 }
