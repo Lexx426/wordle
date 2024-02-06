cb5a701464f06c97358347f7f2a3567e3a265db3
Alexis Tamayo <lexx426@gmail.com>
2024-02-05 18:27:49 -0600

    Debug word bank

--- src/Words.js
+++ src/Words.js
@@ -16,8 +16,11 @@ export const generateWordSet = async () => {
         .then((response) => response.text())
         .then((result) =>{
             const wordArray = result.split("\r\n");
+            console.log(wordArray + "-----")
             todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)]
+            console.log(todaysWord + "''''''''''''''''")
             wordSet = new Set(wordArray)
+            console.log(wordSet + " ======")
         });
     return { wordSet, todaysWord};
 }
