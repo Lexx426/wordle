c87cebc91b994b70bb5dca52295680f2575b7f6e
Alexis Tamayo <lexx426@gmail.com>
2024-02-13 12:19:02 -0600

    array split

--- src/Words.js
+++ src/Words.js
@@ -15,12 +15,15 @@ export const generateWordSet = async () => {
     await fetch(wordBank)
         .then((response) => response.text())
         .then((result) =>{
-            const wordArray = result.split("\r\n");
-            console.log(wordArray + "-----")
+            const wordArray = result.split("\n");
+            // console.log(wordArray + "-----")
+            // wordArray prints
             todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)]
-            console.log(todaysWord + "''''''''''''''''")
+            // console.log(todaysWord + "''''''''''''''''")
+            // todaysWord prints
             wordSet = new Set(wordArray)
-            console.log(wordSet + " ======")
+            // console.log(wordSet + " ======")
+            // wordSet prints out [object set]
         });
     return { wordSet, todaysWord};
 }
